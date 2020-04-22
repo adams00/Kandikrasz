7af305698cb9917da775976619d7ead2629c53ae
Adam <adam.s0@op.pl>
2020-04-22 10:38:36 +0200

    Added selected property to Sweet class

--- src/script/sweet.js
+++ src/script/sweet.js
@@ -6,6 +6,7 @@ export class Sweet {
     this.x = x;
     this.y = y;
     this.width = width;
+    this.selected = false;
   }
   draw() {
     c.drawImage(this.image, this.x, this.y, this.width, this.width);
