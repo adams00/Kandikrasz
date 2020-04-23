8550fc706287a46e90d602f7f09669d07fa00908
Adam <adam.s0@op.pl>
2020-04-23 11:11:01 +0200

    Added click event to document
    It executes toggleSelection function

--- src/script/index.js
+++ src/script/index.js
@@ -1,9 +1,14 @@
 import "../css/style.css";
 import { drawLines, drawAllCandys, setRandomCandys } from "./init.js";
 import { parameters } from "./parameters";
+import { toggleSelection } from "./events.js";
 
 window.onload = () => {
   drawLines(parameters.columns);
   setRandomCandys();
   drawAllCandys();
 };
+document.addEventListener("click", (event) => {
+  toggleSelection(event);
+  drawAllCandys();
+});
