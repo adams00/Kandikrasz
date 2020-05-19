c9fafaedafddd8a1f8e97bd435ec224968b4713a
Adam <adam.s0@op.pl>
2020-05-19 16:24:04 +0200

    Added animations to Sweet class
    There are five new animations: spin and change place for x & y.
    There is also function that disappear candie.
    
    New animations are calculated very time draw function is called.

--- src/script/sweet.js
+++ src/script/sweet.js
@@ -23,10 +23,105 @@ export class Sweet {
     if (this.selected) {
       this.drawSelection();
     }
-    c.drawImage(this.image, this.x, this.y, this.width, this.width);
+    if (this.animation == "disappear") {
+      this.disappear();
+    }
+    if (this.animation == "spinX") {
+      this.calculateSpinX();
+    }
+    if (this.animation == "spinY") {
+      this.calculateSpinY();
+    }
+    if (this.animation == "changePlaceX") {
+      this.changePlaceX();
+    }
+    if (this.animation == "changePlaceY") {
+      this.changePlaceY();
+    }
+    c.drawImage(
+      this.image,
+      (this.x += this.velocityX),
+      (this.y += this.velocityY),
+      this.width,
+      this.width
+    );
   }
   drawSelection() {
     c.fillStyle = parameters.stressColor;
     c.fillRect(this.x, this.y, this.width, this.width);
+  }
+
+  calculateVelocity() {
+    const acceleration = 0.2;
+    if (this.y > this.limit) {
+      this.velocityY = 0;
+    } else {
+      this.velocityY += acceleration;
+    }
+    return this;
+  }
+
+  disappear() {
+    const disappearRate = 0.02;
+    this.width = this.width * (1 - disappearRate);
+    if (this.width < 20) {
+      this.width = 0;
+      this.animation = false;
+      parameters.globalAction = "removingCandies";
+    }
+  }
+
+  calculateSpinX() {
+    const rate = 8;
+    if (this.x === this.limitX) {
+      this.velocityX = -this.velocityX;
+    }
+    if (this.velocityX == 0) {
+      this.startX = this.x;
+      this.velocityX = (this.limitX - this.x) / rate;
+    } else if (this.x === this.startX) {
+      this.velocityX = 0;
+      this.animation = false;
+      parameters.globalAction = false;
+    }
+  }
+
+  calculateSpinY() {
+    const rate = 8;
+    if (this.y == this.limitY) {
+      this.velocityY = -this.velocityY;
+    }
+    if (this.velocityY == 0) {
+      this.startY = this.y;
+      this.velocityY = (this.limitY - this.y) / rate;
+    } else if (this.y == this.startY) {
+      this.velocityY = 0;
+      this.animation = false;
+      parameters.globalAction = false;
+    }
+  }
+
+  changePlaceX() {
+    const rate = 8;
+    if (this.velocityX == 0) {
+      this.velocityX = (this.limitX - this.x) / rate;
+    }
+    if (this.x == this.limitX) {
+      this.velocityX = 0;
+      this.animation = null;
+      parameters.globalAction = "findMaching";
+    }
+  }
+
+  changePlaceY() {
+    const rate = 8;
+    if (this.velocityY == 0) {
+      this.velocityY = (this.limitY - this.y) / rate;
+    }
+    if (this.y == this.limitY) {
+      this.velocityY = 0;
+      this.animation = null;
+      parameters.globalAction = "findMaching";
+    }
   }
 }
